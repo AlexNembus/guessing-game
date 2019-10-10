916e7b72b12e9d90b2fba9635aecb3347fbf46f3
Alex <alexander98mangus@gmail.com>
2019-10-10 17:10:08 +0300

    feat: win guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor(first = null, last = null, assumption = 0) {
+        this.first  = first;
+        this.last   = last;
+        this.assumption = assumption;
+    }
 
     setRange(min, max) {
-
+        this.first = min;
+        this.last  = max;
+        this.assumption = Math.ceil((this.first + this.last)/2);
     }
 
     guess() {
-
+      return this.assumption;
     }
 
     lower() {
-
+      this.last = this.assumption;
+      this.assumption = Math.ceil((this.first + this.last)/2);
     }
 
     greater() {
-
+       this.first = this.assumption;
+       this.assumption =  Math.ceil((this.first + this.last)/2);
     }
 }
 
